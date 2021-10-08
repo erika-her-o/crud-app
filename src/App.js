11174ea70a9f7b97f5bcd90b8c0d14eb6871f3c3
Erika Hernández <74832999+erika-her-o@users.noreply.github.com>
2021-10-08 16:33:43 -0300

    all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UserTable from './components/UserTable';
+import AddUserForm from './components/AddUserForm'
+import {v4 as uuidv4} from 'uuid';
+
 
 function App() {
+
+  const usersData = [
+    { id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
+    { id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+    { id: uuidv4(), name: 'Ben', username: 'benisphere' },
+  ]
+
+  //state
+  const [users, setUsers] = useState(usersData);
+
+  //agregar usuarios
+  const addUser = (user) => {
+    user.id = uuidv4()
+    setUsers([
+      ...users, user
+    ])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>CRUD APP WITH HOOKS</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add User</h2>
+          <AddUserForm addUser={addUser}/>
+        </div>
+        <div className="flex-large">
+          <h2>View Users</h2>
+          <UserTable users={users}/>
+        </div>
+      </div>
     </div>
   );
 }
 
+// video min 26:54
+// https://www.youtube.com/watch?v=8rLs-AGn4go&t=1302s
+// src\components\AddUserForm.jsx
+//  Line 14:9:  'e' is not defined  no-undef
+
+//  Search for the keywords to learn more about each error.
 export default App;
